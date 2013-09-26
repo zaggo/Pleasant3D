26de204b835bd5346374d6315e758904f6b323af
tchapi <regbasket@gmail.com>
2013-09-25 20:16:58 -0400

    Bump new version 2.3.1

--- Sparkle Signing Tools/sign_update.rb
+++ Sparkle Signing Tools/sign_update.rb
@@ -5,3 +5,5 @@ if ARGV.length < 2
 end
 
 puts `openssl dgst -sha1 -binary < "#{ARGV[0]}" | openssl dgst -dss1 -sign "#{ARGV[1]}" | openssl enc -base64`
+
+puts "File size : " << File.stat("#{ARGV[0]}").size.to_s
